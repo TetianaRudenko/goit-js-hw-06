c2d2c5e4ccf91624fe17825346bad599d55dc5de
TetianaRudenko <fyilibratgif@gmail.com>
2023-02-25 21:51:30 +0200

    done task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const colorEl = document.querySelector('span.color');
+
+changeColorBtn.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+})
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
